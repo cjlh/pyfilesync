2cd1ddd23c8ea233ee15081770caf60e99ca533d
Caleb Hamilton <cjlhamilton@gmail.com>
2020-08-05 11:42:04 +0100

    Base functionality implemented. Syncing dirs not working.

--- README.txt
+++ README.txt
@@ -1,9 +1,9 @@
 pyfilesync
 
-What it is
+What it is:
 - Sync selected library directories between computers (e.g. books, music, config files) peer-to-peer
 
-What it is not
+What it isn't:
 - A VCS. No backup or change tracking facilities built in.
 
 Setup:
@@ -15,15 +15,3 @@ Setup:
 - For new local dir:
   * Specify target directory, will build per-dir config folder
   * Then follow above remote step for other PCs
-
-Global config structure
-- Aliases for remote machines
-
-Per-dir config structure
-- ?
-
-- JSON config
-- Remote names are required
-
-- Maybe implement permissions?
-  * Ability to add a folder from any of the clients, and designate write access to all/many/none
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import socket
 import json
 import hashlib
 from functools import partial
+import time
 import tempfile
 from threading import Thread
 
@@ -14,15 +15,30 @@ class Peer():
         self.ip_address = ip_address
         self.port = port
 
-    def recv_file_index(self):
+    def recv_file_index(self, remote_name, lpad=''):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((self.ip_address, self.port))
-        req_data = f'{{"remote_name": "{self.name}"}}'.encode('utf-8')
-        print(f'(send) request: {req_data}')
+        req_data = f'{{"request_type": 0, "remote_name": "{remote_name}"}}'.encode(
+            'utf-8')
+        print(f'{lpad}[send] request to {self.name} at ({self.ip_address}, {self.port}): '
+              f'{req_data}')
         sock.sendall(req_data)
         res_data = sock.recv(1024).decode('utf-8')
-        print(f'(recv) response: {res_data}')
+        print(f'{lpad}[recv] response: {res_data}')
         return json.loads(res_data)
+
+    def recv_file_data(self, remote_name, filepath, lpad=''):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.ip_address, self.port))
+        req_data = f'{{"request_type": 1, "remote_name": "{remote_name}", ' \
+                   f'"filepath": "{filepath}"}}'.encode('utf-8')
+        print(f'{lpad}[send] request to {self.name} at ({self.ip_address}, {self.port}): '
+              f'{req_data}')
+        sock.sendall(req_data)
+        # receive raw bytes
+        res_data = sock.recv(1024)
+        print(f'{lpad}[recv] response: {res_data}')
+        return res_data
 
 
 class AliasStore():
@@ -60,39 +76,88 @@ class Remote():
         return f"remote `{self.name}'" if \
             self.name is not None else 'unnamed remote'
 
-    def update(self, alias_store):
+    def update(self, alias_store, lpad=''):
         # 1: connect to remote hosts, request json-ified FileIndex objects
-        print('Local FileIndex representation:\n  '
-              f'{self.file_index.jsonify()}')
+        print(f'{lpad}Local FileIndex representation: {self.file_index.jsonify()}')
         file_indexes = {}
         for alias in self.peer_aliases:
             peer = alias_store.get_peer(alias)
             try:
-                file_indexes[alias] = peer.recv_file_index()
+                file_indexes[alias] = peer.recv_file_index(self.name, lpad=lpad)
             except (ConnectionRefusedError) as e:
                 # TODO: use e?
-                print(f'Warning: Connection refused by peer {peer.name} for '
-                      f'remote {self.name} at {peer.ip_address}:{peer.port}')
-        # convert to dicts
-        pass
-        # 2: compute changes
-        # get set of all keys from dicts
-        pass
-        # iterate, combine into one dict with latest lm
-        pass
-        # get list of locally changed files to receive
+                print(f'{lpad}Warning: Connection refused by peer `{peer.name}\' for '
+                      f'remote `{self.name}\' at {peer.ip_address}:{peer.port}. Peer may '
+                      'be offline.')
+        if len(file_indexes) == 0:
+            print(f'{lpad}No peers available for update. Abandoning.')
+            return
+        # 2: compute changes between dicts based on md5 hashes
+        # iterate, combine into one dict with latest lm and peer
+        latest_changes = {}
+        for alias, file_index in file_indexes.items():
+            for filename, details in file_index.items():
+                # TODO: check filename is not exploitative
+                # os.dir.join(self.path, filename) is subdir of self.path
+                if (filename not in latest_changes or latest_changes[filename][
+                        'last_modified'] < details['last_modified']):
+                    latest_changes[filename] = {
+                        'md5sum': details['md5sum'],
+                        'last_modified': details['last_modified'],
+                        'peer_alias': alias
+                    }
+            # all_filepaths = all_filepaths.union(set(file_index.keys()))
+        print(f'{lpad}Latest changes from peers: {latest_changes}')
+        # get all filenames
+        all_filepaths = set(latest_changes.keys())
+        for filepath, file in self.file_index.items():
+            all_filepaths.add(filepath)
+        print(f'{lpad}All files: {all_filepaths}')
+        # get list of locally changed files to receive and peer to receive from
         changed_files = []
-        # 3: move old files to temp e.g. /tmp/pyfilesync/{self.dir_name}/
-        tempdir = tempfile.gettempdir()
-        # TODO: exist_ok?
-        os.makedirs(os.path.join(tempdir, 'pyfilesync', self.dir_name),
-                    exist_ok=True)
-        for file in changed_files:
-            # mv
-            pass
-        pass
-        # 4: exchange files as necessary (use L3 proj code) and write
-        pass
+        for filepath in all_filepaths:
+            if self.file_index.is_filepath_in_index(filepath):
+                file = self.file_index.get_file(filepath)
+                if (filepath in latest_changes and
+                        latest_changes[filepath]['last_modified'] > file.get_lm_time() and
+                        latest_changes[filepath]['md5sum'] != file.get_md5sum()):
+                    changed_files.append(filepath)
+            else:
+                changed_files.append(filepath)
+        if len(changed_files) == 0:
+            print(f'{lpad}0 files to update. Done.')
+            return
+        else:
+            print(f"{lpad}{len(changed_files)} "
+                  f"{'file' if len(changed_files) == 1 else 'files'} to update: "
+                  f"{changed_files}")
+        # 3: download to temp folder
+        temp_dir_root = tempfile.gettempdir()
+        temp_dir = os.path.join(temp_dir_root, 'pyfilesync', self.dir_name,
+                                str(get_timestamp()))
+        os.makedirs(temp_dir, exist_ok=True)
+        backup_dir = os.path.join(temp_dir, 'backup')
+        os.makedirs(backup_dir)
+        for filename in changed_files:
+            # TODO: makedirs for files that are in a subdir
+            target_peer = alias_store.get_peer(latest_changes[filename]['peer_alias'])
+            file_data = target_peer.recv_file_data(self.name, filename, lpad=lpad)
+            temp_path = os.path.join(temp_dir, filename)
+            with open(temp_path, 'wb') as f:
+                f.write(file_data)
+            print(f'{lpad}Wrote updated file `{filename}\' to {temp_path} '
+                  f'({len(file_data)} bytes).')
+        # 4: move old files to temp backup folder, move new files to folder
+        for filename in changed_files:
+            # move old if exists
+            target_path = os.path.join(self.path, filename)
+            if os.path.exists(target_path):
+                os.rename(target_path, os.path.join(backup_dir, filename))
+            # move new
+            os.rename(os.path.join(temp_dir, filename), target_path)
+            print(f'{lpad}Updated file `{filename}.\'')
+        # 5: update FileIndex
+        self.file_index.update()
 
 
 class FileIndex():
@@ -108,13 +173,23 @@ class FileIndex():
                                         filename)[len(self.root_dir) + 1:]
                 self.files[filepath] = File(self.root_dir, filepath)
 
+    def is_filepath_in_index(self, filepath):
+        return filepath in self.files
+
+    def get_file(self, filepath):
+        return self.files[filepath]
+
+    def items(self):
+        for filepath, file in self.files.items():
+            yield filepath, file
+
     def print_files(self, lpad=''):
         for path, file in self.files.items():
             print(f'{lpad}{path}: {file.get_lm_time()}, '
                   f'{file.get_md5sum()}')
 
     def jsonify(self):
-        return json.dumps({k: v.jsonify() for k, v in self.files.items()})
+        return json.dumps({k: v.to_dict() for k, v in self.files.items()})
 
 
 class File():
@@ -133,6 +208,10 @@ class File():
         else:
             return self.md5sum
 
+    def read_data(self):
+        with open(self.abs_path, 'rb') as f:
+            return f.read()
+
     def update_md5sum(self):
         with open(self.abs_path, mode='rb') as f:
             d = hashlib.md5()
@@ -140,11 +219,14 @@ class File():
                 d.update(buf)
         self.md5sum = d.hexdigest()
 
-    def jsonify(self):
-        return json.dumps({
+    def to_dict(self):
+        return {
             'md5sum': self.get_md5sum(),
             'last_modified': self.get_lm_time()
-        })
+        }
+
+    def jsonify(self):
+        return json.dumps(self.to_dict())
 
 
 class FileServer(Thread):
@@ -170,13 +252,27 @@ class FileServer(Thread):
             print(f'Connection from {client_addr}')
             data = conn.recv(1024).decode('utf-8')
             parsed_data = json.loads(data)
-            print(f'(recv) request: {parsed_data}')
+            print(f'  [recv] request: {parsed_data}')
             # TODO: validate request
-            remote_name = parsed_data['remote_name']
-            response_data = self.file_indexes[remote_name].jsonify()
-            print(f'(send) response: {response_data}')
-            conn.sendall(response_data.encode('utf-8'))
+            if parsed_data['request_type'] == 0:
+                # request FileIndex
+                remote_name = parsed_data['remote_name']
+                response_data = self.file_indexes[remote_name].jsonify()
+                print(f'  [send] response: {response_data}')
+                conn.sendall(response_data.encode('utf-8'))
+            elif parsed_data['request_type'] == 1:
+                # request file data
+                filepath = parsed_data['filepath']
+                file_data = self.file_indexes[remote_name].get_file(filepath).read_data()
+                print(f'  [send] reponse: {file_data}')
+                conn.sendall(file_data)
+            else:
+                print('  Error: Unrecognised request type. Abandoning.')
             conn.close()
+
+
+def get_timestamp():
+    return int(round(time.time() * 1000))
 
 
 def graceful_exit(status=0):
@@ -190,7 +286,7 @@ def load_config(config_path):
             config = json.load(f)
     except (FileNotFoundError) as e:
         # TODO: use e?
-        print(f'Error: Could not load config file `{config_path}`.')
+        print(f'Error: Could not load config file `{config_path}\'.')
         graceful_exit(1)
 
     # ensure config format valid
@@ -202,7 +298,7 @@ def load_config(config_path):
             print('Error: Unnamed remote found in config.')
             graceful_exit(1)
         elif remote['name'] in remote_names:
-            print(f"Error: Duplicate remote name `{remote['name']}` found.")
+            print(f"Error: Duplicate remote name `{remote['name']}' found.")
             graceful_exit(1)
         remote_names.add(remote['name'])
 
@@ -243,7 +339,7 @@ def main(config_path):
     for remote in remotes:
         print(f'Checking for updates on {remote.get_name_string()}...')
         # remote.file_index.print_files(lpad='  ')
-        remotes[0].update(alias_store)
+        remotes[0].update(alias_store, lpad='  ')
 
     # 5: open thread to listen for update requests
     file_indexes = {}
@@ -254,7 +350,9 @@ def main(config_path):
 
     # 6: start update loop
     while True:
-        pass
+        time.sleep(60 * config['update_interval'])
+        # i:  FileIndex.update
+        # ii: Remote.update
 
 
 if __name__ == '__main__':
